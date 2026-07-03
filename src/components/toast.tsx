59047b68faaf98ed7bd9a6d13964eb18b55d2a77
juji <jujiyangasli@gmail.com>
2026-07-03 18:39:17 +0700

    style(toast): remove unnecessary border styling

--- src/components/toast.tsx
+++ src/components/toast.tsx
@@ -9,7 +9,7 @@ export function Toast() {
 
   return (
     <box position="absolute" top={2} left={2} zIndex={20}>
-      <box backgroundColor={theme.accent.purpleDim} borderStyle="rounded" borderColor={theme.accent.purple} paddingLeft={2} paddingRight={2}>
+      <box backgroundColor={theme.accent.purpleDim} paddingLeft={2} paddingRight={2}>
         <text fg={theme.text.primary}>{message}</text>
       </box>
     </box>
